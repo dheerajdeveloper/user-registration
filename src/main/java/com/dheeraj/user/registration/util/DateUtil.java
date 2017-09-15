e6f09c6f671e73a581f21195877dfa9fe8c202e0
Dheeraj Tyagi <dheeraj.tyagi@paytm.com>
2017-09-15 23:21:44 +0530

    fixing issue with dtae

--- src/main/java/com/dheeraj/user/registration/util/DateUtil.java
+++ src/main/java/com/dheeraj/user/registration/util/DateUtil.java
@@ -1,10 +1,17 @@
 package com.dheeraj.user.registration.util;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Created by dheeraj on 15/09/17.
@@ -26,11 +33,17 @@ public class DateUtil {
             return dateFormat;
         }
     };
+    public static final String DATE_PATTERN_SERVICE = "yyyy-MM-dd HH:mm:ss";
 
 
     public static String getTimeFromLastNMinute(int minute) {
-        return ISO_DATETIME_FORMAT.get().format(new Date(System.currentTimeMillis() - minute * 60 * 1000));
+        DateTimeZone indianZone = DateTimeZone.forID("Asia/Kolkata");
+        DateTimeFormatter indianZoneFormatter = DateTimeFormat
+                .forPattern(DATE_PATTERN_SERVICE)
+                .withLocale(Locale.US)
+                .withZone(indianZone);
 
-
+        DateTime parsed = DateTime.now(indianZone).minusMinutes(minute);
+        return indianZoneFormatter.print(parsed);
     }
 }
